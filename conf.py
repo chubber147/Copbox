b916d6f722d577a75ebf6200e4a9897f84fba78e
chubber147 <chubber147@users.noreply.github.com>
2020-03-16 15:26:00 +0100

    Update conf.py

--- conf.py
+++ conf.py
@@ -102,7 +102,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # show_authors = False
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+pygments_style = 'sphinx_rtd_theme'
 
 # A list of ignored prefixes for module index sorting.
 # modindex_common_prefix = []
